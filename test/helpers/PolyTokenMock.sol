e9880a64b49decaff2ffe6e5792dfa14cb5f7662
Sukhveer Sanghera <sukhveer@ledgerx.com>
2017-10-30 11:04:50 -0400

    Fix test

--- test/helpers/PolyTokenMock.sol
+++ test/helpers/PolyTokenMock.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.15;
 
-import '../../contracts/ERC20.sol';
+import '../../contracts/PolyToken.sol';
+import '../../contracts/interfaces/IERC20.sol';
 
-contract ERC20Mock is ERC20 {
+contract PolyToken is IERC20 {
     function assign(address _account, uint _balance) {
         balances[_account] = _balance;
     }
