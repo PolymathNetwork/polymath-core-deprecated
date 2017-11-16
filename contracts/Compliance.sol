b3b1ee4ca6a97b0b35cc8dfe807f0d621a149b92
Arthur L Lunn <ArthurLLunn@gmail.com>
2017-11-16 15:26:47 +0000

    Lint Compliance and IERC20

--- contracts/Compliance.sol
+++ contracts/Compliance.sol
@@ -11,110 +11,145 @@ import './Ownable.sol';
 
 contract Compliance is Ownable {
 
-  string public VERSION = '0.1';
+    string public VERSION = "0.1";
 
-  // A compliance template
-  struct Template {
-    address owner;
-    string securityType;
-    bytes32 issuerJurisdiction;
-    mapping (bytes32 => bool) allowedJurisdictions;
-    bool accredited;
-    bytes32 complianceProcess;
-    uint256 finalizes;
-    uint256 expires;
-    uint256 fee;
-  }
-
-  // All compliance templates are stored in a mapping
-  mapping(bytes32 => Template) templates;
-
-  // A legal delegate
-  struct Delegate {
-    bytes32 details;
-    bytes32[] jurisdictions;
-  }
-
-  // All legal delegates are stored in a mapping
-  mapping(address => Delegate) public delegates;
-
-  // Notifications
-  event NewDelegate(address _delegateAddress, bytes32[] _jurisdictions, bytes32 _details);
-  event TemplateCreated(address creator, bytes32 _template, string _securityType);
-
-  /// Allow new legal delegates to join Polymath
-  /// @param _delegateAddress The legal delegate's public key address
-  /// @param _details A SHA256 hash of the application document i.e. IPFS url
-  /// @param _jurisdictions An array of jurisdictions they specialize in
-  function newDelegate(address _delegateAddress, bytes32 _details, bytes32[] _jurisdictions) {
-    require(_delegateAddress != address(0));
-    require(delegates[_delegateAddress].details == 0);
-    delegates[_delegateAddress].details = _details;
-    delegates[_delegateAddress].jurisdictions = _jurisdictions;
-    NewDelegate(_delegateAddress, _jurisdictions, _details);
-  }
-
-  /// Create a new compliance template
-  /// @param _template A SHA256 hash of the JSON schema containing full compliance process/requirements
-  /// @param _securityType The type of security being issued
-  /// @param _issuerJurisdiction The jurisdiction id of the issuer
-  /// @param _finalizes Timestamp of when the template will finalize and become non-editable
-  /// @param _expires Timestamp of when the template will expire
-  /// @param _fee Amount of POLY to use the template (held in escrow until issuance)
-  function createTemplate(
-    bytes32 _template,
-    string _securityType,
-    bytes32 _issuerJurisdiction,
-    bool _accredited,
-    uint256 _finalizes,
-    uint256 _expires,
-    uint256 _fee
-  ) {
-    require(delegates[msg.sender].details != 0);
-    require(templates[_template].owner == address(0));
-    require(_finalizes > now);
-    require(_expires >= _finalizes);
-    templates[_template].owner = msg.sender;
-    templates[_template].issuerJurisdiction = _issuerJurisdiction;
-    templates[_template].securityType = _securityType;
-    templates[_template].fee = _fee;
-    templates[_template].accredited = _accredited;
-    templates[_template].finalizes = _finalizes;
-    templates[_template].expires = 0;
-    TemplateCreated(msg.sender, _template, _securityType);
-  }
-
-  /// Allow adding of new jurisdictions to a template
-  /// @param _template A SHA256 hash of the JSON schema containing full compliance process/requirements
-  /// @param _allowedJurisdictions An array of jurisdictions
-  /// @param _allowed An array of whether the jurisdiction is allowed to purchase the security or not
-  function addJurisdictionToTemplate(bytes32 _template, bytes32[] _allowedJurisdictions, bool[] _allowed) {
-    require(templates[_template].owner == msg.sender);
-    require(templates[_template].finalizes > now);
-    for(uint i = 0; i < _allowedJurisdictions.length; ++i) {
-      templates[_template].allowedJurisdictions[_allowedJurisdictions[i]] = _allowed[i];
+    // A compliance template
+    struct Template {
+        address owner;
+        string securityType;
+        bytes32 issuerJurisdiction;
+        mapping (bytes32 => bool) allowedJurisdictions;
+        bool accredited;
+        bytes32 complianceProcess;
+        uint256 finalizes;
+        uint256 expires;
+        uint256 fee;
     }
-  }
 
-  /// Finalize template
-  /// @param _template A SHA256 hash of the JSON schema containing full compliance process/requirements
-  function finalizeTemplate(bytes32 _template) {
-    require(templates[_template].owner == msg.sender);
-    require(templates[_template].finalizes > now);
-    templates[_template].finalizes = now;
-  }
+    // All compliance templates are stored in a mapping
+    mapping(bytes32 => Template) templates;
 
-  /// Check if template requirements are met
-  /// @param _template A SHA256 hash of the JSON schema containing full compliance process/requirements
-  /// @param _jurisdiction The ISO-3166 code of the investors jurisdiction
-  /// @param _accredited Whether the investor is accredited or not
-  function checkTemplateRequirements(bytes32 _template, bytes32 _jurisdiction, bool _accredited) returns (bool allowed) {
-    require(_template != 0 && _jurisdiction != 0);
-    require(templates[_template].allowedJurisdictions[_jurisdiction]);
-    if (templates[_template].accredited) {
-      require(_accredited);
+    // A legal delegate
+    struct Delegate {
+        bytes32 details;
+        bytes32[] jurisdictions;
     }
-    return true;
-  }
+
+    // All legal delegates are stored in a mapping
+    mapping(address => Delegate) public delegates;
+
+    // Notifications
+    event NewDelegate(
+        address _delegateAddress,
+        bytes32[] _jurisdictions,
+        bytes32 _details
+    );
+    event TemplateCreated(
+        address creator,
+        bytes32 _template,
+        string _securityType
+    );
+
+    /// Allow new legal delegates to join Polymath
+    /// @param _delegateAddress The legal delegate's public key address
+    /// @param _details A SHA256 hash of the application document i.e. IPFS url
+    /// @param _jurisdictions An array of jurisdictions they specialize in
+    function newDelegate(
+        address _delegateAddress, 
+        bytes32 _details, 
+        bytes32[] _jurisdictions
+    ) public
+    {
+        require(_delegateAddress != address(0));
+        require(delegates[_delegateAddress].details == 0);
+        delegates[_delegateAddress].details = _details;
+        delegates[_delegateAddress].jurisdictions = _jurisdictions;
+        NewDelegate(_delegateAddress, _jurisdictions, _details);
+    }
+
+    /// `createTemplate` is a simple function to create
+    ///  a new compliance template
+    /// @param _template A SHA256 hash of the JSON schema containing full
+    ///  compliance process/requirements
+    /// @param _securityType The type of security being issued
+    /// @param _issuerJurisdiction The jurisdiction id of the issuer
+    /// @param _finalizes Timestamp of when the template will finalize
+    ///  and become non-editable
+    /// @param _expires Timestamp of when the template will expire
+    /// @param _fee Amount of POLY to use the template (held in
+    ///  escrow until issuance)
+    function createTemplate(
+        bytes32 _template,
+        string _securityType,
+        bytes32 _issuerJurisdiction,
+        bool _accredited,
+        uint256 _finalizes,
+        uint256 _expires,
+        uint256 _fee
+    ) public
+    {
+        require(delegates[msg.sender].details != 0);
+        require(templates[_template].owner == address(0));
+        require(_finalizes > now);
+        require(_expires >= _finalizes);
+        templates[_template].owner = msg.sender;
+        templates[_template].issuerJurisdiction = _issuerJurisdiction;
+        templates[_template].securityType = _securityType;
+        templates[_template].fee = _fee;
+        templates[_template].accredited = _accredited;
+        templates[_template].finalizes = _finalizes;
+        templates[_template].expires = 0;
+        TemplateCreated(msg.sender, _template, _securityType);
+    }
+
+    /// @notice `addJurisdictionToTemplate`allows the adding of new
+    ///  jurisdictions to a template
+    /// @param _template A SHA256 hash of the JSON schema containing full
+    ///  compliance process/requirements
+    /// @param _allowedJurisdictions An array of jurisdictions
+    /// @param _allowed An array of whether the jurisdiction is allowed to
+    ///  purchase the security or not
+    function addJurisdictionToTemplate(
+        bytes32 _template, 
+        bytes32[] _allowedJurisdictions, 
+        bool[] _allowed
+    ) public
+    {
+        require(templates[_template].owner == msg.sender);
+        require(templates[_template].finalizes > now);
+        for (uint i = 0; i < _allowedJurisdictions.length; ++i) {
+            templates[_template]
+            .allowedJurisdictions[_allowedJurisdictions[i]] = _allowed[i];
+        }
+    }
+
+    /// `finalizeTemplate` is used to finalize template.
+    /// @param _template A SHA256 hash of the JSON schema containing
+    ///  full compliance process/requirements
+    function finalizeTemplate(bytes32 _template) public {
+        require(templates[_template].owner == msg.sender);
+        require(templates[_template].finalizes > now);
+        templates[_template].finalizes = now;
+    }
+
+    /// `checkTemplateRequirements` is a constant function that 
+    ///  checks if templates requirements are met
+    /// @param _template A SHA256 hash of the JSON schema containing full 
+    ///  compliance process/requirements
+    /// @param _jurisdiction The ISO-3166 code of the investors jurisdiction
+    /// @param _accredited Whether the investor is accredited or not
+    function checkTemplateRequirements(
+        bytes32 _template,
+        bytes32 _jurisdiction,
+        bool _accredited
+    ) public constant returns (bool allowed)
+    {
+        require(_template != 0 && _jurisdiction != 0);
+        require(templates[_template].allowedJurisdictions[_jurisdiction]);
+        if (templates[_template].accredited) {
+            require(_accredited);
+        }
+        return true;
+    }
 
 }
