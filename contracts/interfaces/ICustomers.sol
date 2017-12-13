b708854e80513440ab9d9593d01dabdeb4ecc635
Sukhveer Sanghera <sukhveer@ledgerx.com>
2017-12-13 12:07:11 -0400

    Fix compilation bug

--- contracts/interfaces/ICustomers.sol
+++ contracts/interfaces/ICustomers.sol
@@ -1,37 +1,33 @@
 pragma solidity ^0.4.18;
 
 contract ICustomers {
-  /** @dev Allow new provider applications
-    @param _providerAddress The provider's public key address
-    @param _name The provider's name
-    @param _details A SHA256 hash of the new providers details
-    @param _fee The fee charged for customer verification */
+
+  /* @dev Allow new provider applications
+  @param _providerAddress The provider's public key address
+  @param _name The provider's name
+  @param _details A SHA256 hash of the new providers details
+  @param _fee The fee charged for customer verification */
   function newProvider(address _providerAddress, string _name, bytes32 _details, uint256 _fee) public returns (bool success);
 
-  /** @dev Allow new investor applications
-     @param _jurisdiction The jurisdiction code of the customer
-     @param _provider The provider selected by the customer to do verification
-     @param _role The type of customer - investor:1, issuer:2, delegate:3
-     @param _proof The SHA256 hash of the documentation provided to prove identity */
-  function newCustomer(bytes32 _jurisdiction, address _provider, uint8 _role, bytes32 _proof) public returns (bool success);
+  /* @dev Change a providers fee
+  @param _newFee The new fee of the provider */
+  function changeFee(uint256 _newFee) public returns (bool success);
 
-  /** @dev Verify an investor
-    @param _customer The customer's public key address
-    @param _jurisdiction The jurisdiction code of the customer
-    @param _role The type of customer - investor:1, issuer:2, delegate:3, marketmaker:4,
-    @param _accredited Whether the customer is accredited or not (only applied to investors)
-    @param _proof The SHA256 hash of the documentation provided to prove identity
-    @param _expires The time the verification expires */
+  /* @dev Verify an investor
+  @param _customer The customer's public key address
+  @param _jurisdiction The jurisdiction code of the customer
+  @param _role The type of customer - investor:1, issuer:2, delegate:3, marketmaker:4, etc.
+  @param _accredited Whether the customer is accredited or not (only applied to investors)
+  @param _expires The time the verification expires */
   function verifyCustomer(
-      address _customer,
-      bytes32 _jurisdiction,
-      uint8 _role,
-      bool _accredited,
-      bytes32 _proof,
-      uint256 _expires
+    address _customer,
+    bytes32 _jurisdiction,
+    uint8 _role,
+    bool _accredited,
+    uint256 _expires
   ) public returns (bool success);
 
-  /// Getter function for attestations
+  // Get customer attestation data by KYC provider and customer ethereum address
   function getCustomer(address _provider, address _customer) public constant returns (
     bytes32,
     bool,
@@ -39,11 +35,12 @@ contract ICustomers {
     bool,
     uint256
   );
-  
+
+  // Get provider details and fee by ethereum address
   function getProvider(address _providerAddress) public constant returns (
-        string name,
-        uint256 joined,
-        bytes32 details,
-        uint256 fee
-    );
+    string name,
+    uint256 joined,
+    bytes32 details,
+    uint256 fee
+  );
 }
