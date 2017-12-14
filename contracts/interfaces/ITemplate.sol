dc6c6563d3e0aca82a3aa53241c9f779d5c404d4
Sukhveer Sanghera <sukhveer@ledgerx.com>
2017-12-14 08:33:20 -0500

    Fixes #97

--- contracts/interfaces/ITemplate.sol
+++ contracts/interfaces/ITemplate.sol
@@ -2,42 +2,34 @@ pragma solidity ^0.4.18;
 
 interface ITemplate {
 
-  /* @dev `addJurisdictionToTemplate`allows the adding of new
-  jurisdictions to a template
-  @param _template A SHA256 hash of the JSON schema containing full
-  compliance process/requirements
+  /* @dev `addJurisdiction` allows the adding of new jurisdictions to a template
   @param _allowedJurisdictions An array of jurisdictions
-  @param _allowed An array of whether the jurisdiction is allowed to
-  purchase the security or not */
-  function addJurisdictionToTemplate(bytes32 _template, bytes32[] _allowedJurisdictions, bool[] _allowed) public;
+  @param _allowed An array of whether the jurisdiction is allowed to purchase the security or not */
+  function addJurisdiction(bytes32[] _allowedJurisdictions, bool[] _allowed) public;
 
-  /* @dev `addRoleToTemplate` allows the adding of new roles to be added to whitelist
-  @param _template A SHA256 hash of the JSON schema containing full compliance process/requirements
+  /* @dev `addRole` allows the adding of new roles to be added to whitelist
   @param _allowedRoles User roles that can purchase the security */
-  function addRolesToTemplate(bytes32 _template, uint8[] _allowedRoles) public;
+  function addRoles(uint8[] _allowedRoles) public;
 
-  /* @dev `finalizeTemplate` is used to finalize template.
-  @param _template A SHA256 hash of the JSON schema containing
-  full compliance process/requirements */
-  function finalizeTemplate(bytes32 _template) public;
+  /// @notice `updateDetails`
+  function updateDetails(bytes32 _details) public returns (bool allowed);
 
-  /* @dev `checkTemplateRequirements` is a constant function that
-  checks if templates requirements are met
+  /* @dev `finalizeTemplate` is used to finalize template.full compliance process/requirements */
+  function finalizeTemplate() public returns (bool success);
+
+  /* @dev `checkTemplateRequirements` is a constant function that checks if templates requirements are met
   @param _jurisdiction The ISO-3166 code of the investors jurisdiction
-  @param _accredited Whether the investor is accredited or not
-  @param _role Check that the role is permitted */
+  @param _accredited Whether the investor is accredited or not */
   function checkTemplateRequirements(
-    bytes32 _jurisdiction,
-    bool _accredited,
-    uint8 _role
+      bytes32 _jurisdiction,
+      bool _accredited,
+      uint8 _role
   ) public constant returns (bool allowed);
 
-  /// `getUsageDetails` is a function to get all the details on template usage fees
-  function getUsageDetails() public constant returns (
-    uint256 fee,
-    uint8 quorum,
-    uint256 vestingPeriod,
-    address owner,
-    address KYC
-  );
+  /* getTemplateDetails is a constant function that gets template details
+  @return bytes32 details, bool finalized */
+  function getTemplateDetails() view public returns (bytes32, bool);
+
+  /// `getUsageFees` is a function to get all the details on template usage fees
+  function getUsageDetails() view public returns (uint256, uint8, uint256, address, address);
 }
