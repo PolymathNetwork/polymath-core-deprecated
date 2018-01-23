e97ce7c58aabceff53cef9c61324f961b3b3d187
Pablo Ruiz <pabloruiz55@gmail.com>
2018-01-23 09:19:19 -0300

    reverted addToWhitelist require.

--- contracts/SecurityToken.sol
+++ contracts/SecurityToken.sol
@@ -237,7 +237,7 @@ contract SecurityToken is IERC20 {
      * @return bool success
      */
     function addToWhitelist(address _whitelistAddress) public returns (bool success) {
-        require(KYC == msg.sender);
+        require(KYC == msg.sender || owner == msg.sender);
         var (jurisdiction, accredited, role, verified, expires) = PolyCustomers.getCustomer(KYC, _whitelistAddress);
         require(verified && expires > now);
         require(Template.checkTemplateRequirements(jurisdiction, accredited, role));
