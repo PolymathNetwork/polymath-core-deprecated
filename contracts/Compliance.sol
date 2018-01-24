43b7655a1e18dce9bff0e87dd587c40035580a8e
satyam <satyam@secureblocks.io>
2018-01-24 20:35:32 +0530

    fix typo error

--- contracts/Compliance.sol
+++ contracts/Compliance.sol
@@ -48,7 +48,7 @@ contract Compliance is ICompliance {
 
     Customers public PolyCustomers;                                      // Instance of the Compliance contract
     uint256 public constant MINIMUM_VESTING_PERIOD = 60 * 60 * 24 * 100; // 100 Day minimum vesting period for POLY earned
-    bool hasSTRAdded = false;
+    bool hasSTRAdded = false;                                            // This boolean variable use to restrict to reset the STR address again
 
     // Notifications
     event LogTemplateCreated(address indexed _creator, address _template, string _offeringType);
@@ -61,13 +61,12 @@ contract Compliance is ICompliance {
     }
 
     /**
-     * @dev Used to set the address of the SecurityTokenRegistrar contract
-     * This function is only be called once 
-     * @param _STRegistrar Addres of the STR contract
+     * @dev `setRegsitrarAddress` This function set the SecurityTokenRegistrar contract address. 
+     * @param _STRegistrar It is the `this` reference of STR contract
      * @return bool
      */
 
-    function setSTRegsitrar(address _STRegistrar) public returns (bool) {
+    function setRegsitrarAddress(address _STRegistrar) public returns (bool) {
         require(!hasSTRAdded);
         STRegistrar = SecurityTokenRegistrar(_STRegistrar);
         hasSTRAdded = !hasSTRAdded;
