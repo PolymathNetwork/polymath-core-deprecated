d9ff6ef6ef847d9f6b03c783953f03cde0a40f34
Adam Dossa <adam.dossa@gmail.com>
2018-02-11 21:29:59 +0000

    Change gas price

--- truffle.js
+++ truffle.js
@@ -16,7 +16,7 @@ const config = {
       network_id: '*',
       // from: '0x00F13d5bCA2E8A4E58fD8018a7b1e8D286dD135A',
       gas: 4700036,
-      gasPrice: 20000000000,
+      gasPrice: 130000000000,
     },
     coverage: {
       host: 'localhost',
