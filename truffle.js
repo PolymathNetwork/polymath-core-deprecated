6b9a457022de7b7742ba09db7cc111b8af6e4db3
Adam Dossa <adam.dossa@gmail.com>
2018-02-11 21:30:28 +0000

    Add from address

--- truffle.js
+++ truffle.js
@@ -14,7 +14,7 @@ const config = {
       host: 'localhost',
       port: 8545,
       network_id: '*',
-      // from: '0x00F13d5bCA2E8A4E58fD8018a7b1e8D286dD135A',
+      from: '0x00F13d5bCA2E8A4E58fD8018a7b1e8D286dD135A',
       gas: 4700036,
       gasPrice: 130000000000,
     },
