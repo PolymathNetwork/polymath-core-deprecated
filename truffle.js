8d47c9a4a49d073a7aafd9466dbac40dc136a4c9
Sukhveer Sanghera <sukhveer@ledgerx.com>
2018-02-13 13:36:57 -0500

    Update deploy process

--- truffle.js
+++ truffle.js
@@ -1,8 +1,8 @@
 require('babel-register');
 require('babel-polyfill');
 const WalletProvider = require("truffle-wallet-provider");
-const keystore = require('fs').readFileSync('./UTC--2018-01-26T17-50-04Z--074bf411-7666-c662-f1a0-d6a93f5d8719').toString();
-const pass = require('fs').readFileSync('./password1.txt').toString();
+const keystore = require('fs').readFileSync('./sample-keystore').toString();
+const pass = require('fs').readFileSync('./sample-pass').toString();
 const wallet = require('ethereumjs-wallet').fromV3(keystore, pass);
 
 const config = {
@@ -42,22 +42,4 @@ const config = {
   },
 };
 
-// Comment line below and uncomment the lines going after it to use Infura for deployment
 module.exports = config;
-
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-//
-// const mnemonic = '';
-// const infuraToken = ''; // https://infura.io/
-//
-// config.networks = {
-//   ropsten: {
-//     provider: function() {
-//       return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/' + infuraToken)
-//     },
-//     network_id: config.networks.ropsten.network_id,
-//     gas: config.networks.ropsten.gas,
-//   }
-// };
-//
-// module.exports = config;
