2e67b32b7d8e941078792088b9aa2510bfaa135a
satyam <satyam@secureblocks.io>
2018-02-20 11:46:02 +0530

    fix #176

--- contracts/interfaces/ICompliance.sol
+++ contracts/interfaces/ICompliance.sol
@@ -74,8 +74,8 @@ interface ICompliance {
         uint256 _templateProposalIndex
     ) public returns (bool success);
 
-    /**
-     * @dev Set the STO contract by the issuer.
+   /**
+     * @dev Register the Offering factory by the developer.
      * @param _factoryAddress address of the offering factory
      * @return bool success
      */
@@ -84,9 +84,9 @@ interface ICompliance {
     ) public returns (bool success);
 
     /**
-     * @dev Cancel a STO contract proposal if the bid hasn't been accepted
+     * @dev Cancel a Offering factory proposal if the bid hasn't been accepted
      * @param _securityToken The security token being bid on
-     * @param _offeringFactoryProposalIndex The offering proposal array index
+     * @param _offeringFactoryProposalIndex The offeringFactory proposal array index
      * @return bool success
      */
     function cancelOfferingFactoryProposal(
