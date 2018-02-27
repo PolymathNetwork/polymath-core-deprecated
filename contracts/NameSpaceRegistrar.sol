cdb3e64accc6f41c85caf887738993876d3cf7d8
Adam Dossa <adam.dossa@gmail.com>
2018-02-27 11:37:57 +0000

    Make code cleaner

--- contracts/NameSpaceRegistrar.sol
+++ contracts/NameSpaceRegistrar.sol
@@ -16,12 +16,15 @@ contract NameSpaceRegistrar {
 
     string public VERSION = "2";
 
-    mapping (string => mapping (string => address)) symbolOwner;          //Maps namespaces to token symbols to owner address
-    mapping (string => mapping (string => uint256)) symbolTimestamp;      //Maps namespaces to token symbols to registration timestamp
-    mapping (string => mapping (string => string)) symbolDescription;     //Maps token symbols to their description - this is only used internally within this contract
-    mapping (string => mapping (string => string)) symbolContact;         //Maps token symbols to their contact details - this is only used internally within this contract
-    mapping (address => bool) hasRegistered;                              //Tracks addresses that have registered tokens, only allow one registration per address
-    mapping (address => bool) public admins;                              //Track valid admin addresses
+    struct SymbolDetails {
+      address owner;
+      uint256 timestamp;
+      string description;
+    }
+
+    mapping (string => mapping (string => SymbolDetails)) symbolDetails;    //Tracks details for namespace / symbol registrations
+    mapping (address => bool) hasRegistered;                                //Tracks addresses that have registered tokens, only allow one registration per address
+    mapping (address => bool) public admins;                                //Track valid admin addresses
 
     event AdminChange(address indexed _admin, bool _valid);
     event RegisteredToken(string _nameSpace, string _symbol, string _description, string _contact, address indexed _owner, address indexed _admin, uint256 _timestamp);
@@ -61,8 +64,8 @@ contract NameSpaceRegistrar {
      * @param _newOwner new owner
      */
     function changeTokenOwner(string _nameSpace, string _symbol, address _newOwner) public {
-      require(symbolOwner[_nameSpace][_symbol] == msg.sender);
-      symbolOwner[_nameSpace][_symbol] = _newOwner;
+      require(symbolDetails[_nameSpace][_symbol].owner == msg.sender);
+      symbolDetails[_nameSpace][_symbol].owner = _newOwner;
       TokenOwnerChange(_nameSpace, _symbol, msg.sender, _newOwner);
     }
 
@@ -76,12 +79,12 @@ contract NameSpaceRegistrar {
      */
     function registerToken(string _nameSpace, string _symbol, string _description, string _contact, address _owner) onlyAdmin public {
       require(!hasRegistered[_owner]);
-      require(symbolOwner[_nameSpace][_symbol] == address(0));
+      require(_owner != address(0));
+      require(symbolDetails[_nameSpace][_symbol].owner == address(0));
       hasRegistered[_owner] = true;
-      symbolOwner[_nameSpace][_symbol] = _owner;
-      symbolDescription[_nameSpace][_symbol] = _description;
-      symbolContact[_nameSpace][_symbol] = _contact;
-      symbolTimestamp[_nameSpace][_symbol] = now;
+      symbolDetails[_nameSpace][_symbol].owner = _owner;
+      symbolDetails[_nameSpace][_symbol].description = _description;
+      symbolDetails[_nameSpace][_symbol].timestamp = now;
       RegisteredToken(_nameSpace, _symbol, _description, _contact, _owner, msg.sender, now);
     }
 
@@ -91,7 +94,7 @@ contract NameSpaceRegistrar {
      * @param _symbol symbol
      */
     function getDetails(string _nameSpace, string _symbol) view public returns (address, uint256) {
-      return (symbolOwner[_nameSpace][_symbol], symbolTimestamp[_nameSpace][_symbol]);
+      return (symbolDetails[_nameSpace][_symbol].owner, symbolDetails[_nameSpace][_symbol].timestamp);
     }
 
     /**
@@ -99,8 +102,8 @@ contract NameSpaceRegistrar {
      * @param _nameSpace namespace
      * @param _symbol symbol
      */
-    function getDescription(string _nameSpace, string _symbol) view public returns (string, string) {
-      return (symbolDescription[_nameSpace][_symbol], symbolDescription[_nameSpace][_symbol]);
+    function getDescription(string _nameSpace, string _symbol) view public returns (string) {
+      return symbolDetails[_nameSpace][_symbol].description;
     }
 
 }
